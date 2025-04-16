ec400815c8d2d7b33e099ea3f07b2f15e3d22eb1
SAYAN ROY <mrsayan7908@gmail.com>
2025-04-16 16:03:11 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    var typed = new Typed(".typing", {
+        strings: ["Senior Research Fellow",  "department of Basic Science and Humanities,"],
+        typeSpeed: 50,
+        backSpeed: 40,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: [ "Senior Research Fellow", "department of Basic Science and Humanities,"],
+        typeSpeed: 60,
+        backSpeed: 40,
+        loop: true
+    });  
+});
